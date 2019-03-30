4be2b3fadcc8a15767aff316f160cea6e47dfd67
Santosh <santosh@stelcore.in>
2019-03-30 09:01:24 +0530

    done with activity fields

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ActivityfeedComponent} from "./pages/activityfeed/activityfeed.component";
+import {HightlightComponent} from "./pages/hightlight/hightlight.component";
+import {WorldmapComponent} from "./pages/worldmap/worldmap.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'activity', component: ActivityfeedComponent },
+  { path: 'highlight', component: HightlightComponent },
+  { path: 'worldmap', component: WorldmapComponent },
+  { path: '', redirectTo: '/activity', pathMatch: 'full' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HightlightComponent } from './pages/hightlight/hightlight.component';
+import { ActivityfeedComponent } from './pages/activityfeed/activityfeed.component';
+import { WorldmapComponent } from './pages/worldmap/worldmap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HightlightComponent,
+    ActivityfeedComponent,
+    WorldmapComponent
   ],
   imports: [
     BrowserModule,
